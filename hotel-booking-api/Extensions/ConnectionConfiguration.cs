890ac84385a7a43f6a49888edc6d7f692dfde703
reactK1n <adewings1995@gmail.com>
2024-03-25 14:38:39 +0100

    Add project files.

---
+++ hotel-booking-api/Extensions/ConnectionConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using hotel_booking_data.Contexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace hotel_booking_api.Extensions
+{
+    public static class ConnectionConfiguration
+    {
+
+        public static void AddDbContextAndConfigurations(this IServiceCollection services, IWebHostEnvironment env, IConfiguration config)
+        {
+            services.AddDbContextPool<HbaDbContext>(options =>
+            {
+                string connStr;
+/*
+                if (env.IsProduction())
+                {
+                    connStr = GetHerokuConnectionString();
+                }
+                else
+                {*/
+                    connStr = config.GetConnectionString("PostgresConnectionString");
+                
+                options.UseNpgsql(connStr);
+            });
+        }
+    }
+}
